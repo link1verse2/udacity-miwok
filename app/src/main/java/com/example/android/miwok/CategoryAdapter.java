677aee684ddbe5cf78c1762d335aaf06ef18d53d
unnyjoh <bird.smithe@gmail.com>
2017-12-08 22:43:44 -0800

    Add tabs to categories

--- app/src/main/java/com/example/android/miwok/CategoryAdapter.java
+++ app/src/main/java/com/example/android/miwok/CategoryAdapter.java
@@ -1,5 +1,6 @@
 package com.example.android.miwok;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -10,38 +11,52 @@ import android.support.v4.app.FragmentPagerAdapter;
 
 public class CategoryAdapter extends FragmentPagerAdapter {
 
-    public CategoryAdapter(FragmentManager fm) {
-        super(fm);
-    }
+    final int PAGE_COUNT = 4;
+    private String tabTitles[] = new String[] { "Numbers", "Colors", "Family", "Phrases" };
+    private Context context;
 
-    @Override
-    public Fragment getItem(int position) {
-        if (position == 0) {
-//            DayFragment mon = new DayFragment();
-//            mon.setDay("Monday");
-            return new NumbersFragment();
-        }
-        else if (position == 1) {
-//            DayFragment tue = new DayFragment();
-//            tue.setDay("Tuesday");
-//            return tue;
-            return new FamilyFragment();
-        } else if (position == 2) {
-//            DayFragment wed = new DayFragment();
-//            wed.setDay("Wednesday");
-//            return wed;
-            return new ColorsFragment();
-        }
-        else {
-//            DayFragment fri = new DayFragment();
-//            fri.setDay("Friday");
-//            return fri;
-            return new PhraseFragment();
-        }
+    public CategoryAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        this.context = context;
     }
 
     @Override
     public int getCount() {
-        return 4;
+        return PAGE_COUNT;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        if (position == 0) {
+            return context.getString(R.string.category_numbers);
+        }
+        else if (position == 1) {
+            return context.getString(R.string.category_colors);
+        } else if (position == 2) {
+            return context.getString(R.string.category_family);
+        }
+        else {
+            return context.getString(R.string.category_phrases);
+        }
+    }
+
+//    public CategoryAdapter(FragmentManager fm) {
+//        super(fm);
+//    }
+//
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new NumbersFragment();
+        }
+        else if (position == 1) {
+            return new FamilyFragment();
+        } else if (position == 2) {
+            return new ColorsFragment();
+        }
+        else {
+            return new PhraseFragment();
+        }
     }
 }
--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.miwok;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
@@ -15,17 +16,32 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
 
-        // Find the view pager that will allow the user to swipe between fragments
+        // Get the ViewPager and set it's PagerAdapter so that it can display items
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new CategoryAdapter(getSupportFragmentManager(),
+                MainActivity.this));
 
-        // Create an adapter that knows which fragment should be shown on each page
-        CategoryAdapter adapter = new CategoryAdapter(getSupportFragmentManager());
-
-        // Set the adapter onto the view pager
-        viewPager.setAdapter(adapter);
+        // Give the TabLayout the ViewPager
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//
+//        // Set the content of the activity to use the activity_main.xml layout file
+//        setContentView(R.layout.activity_main);
+//
+//        // Find the view pager that will allow the user to swipe between fragments
+//        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+//
+//        // Create an adapter that knows which fragment should be shown on each page
+//        CategoryAdapter adapter = new CategoryAdapter(getSupportFragmentManager());
+//
+//        // Set the adapter onto the view pager
+//        viewPager.setAdapter(adapter);
+//    }
 }
