28bb2c3e9b90c686fcd426181f87a8c60366e3ec
unnyjoh <bird.smithe@gmail.com>
2017-12-08 23:05:13 -0800

    Add style to tabs

--- app/src/main/java/com/example/android/miwok/CategoryAdapter.java
+++ app/src/main/java/com/example/android/miwok/CategoryAdapter.java
@@ -12,7 +12,6 @@ import android.support.v4.app.FragmentPagerAdapter;
 public class CategoryAdapter extends FragmentPagerAdapter {
 
     final int PAGE_COUNT = 4;
-    private String tabTitles[] = new String[] { "Numbers", "Colors", "Family", "Phrases" };
     private Context context;
 
     public CategoryAdapter(FragmentManager fm, Context context) {
@@ -32,9 +31,9 @@ public class CategoryAdapter extends FragmentPagerAdapter {
             return context.getString(R.string.category_numbers);
         }
         else if (position == 1) {
-            return context.getString(R.string.category_colors);
-        } else if (position == 2) {
             return context.getString(R.string.category_family);
+        } else if (position == 2) {
+            return context.getString(R.string.category_colors);
         }
         else {
             return context.getString(R.string.category_phrases);
--- app/src/main/java/com/example/android/miwok/NumbersFragment.java
+++ app/src/main/java/com/example/android/miwok/NumbersFragment.java
@@ -1,18 +1,18 @@
 package com.example.android.miwok;
 
-        import android.support.v4.app.Fragment;
-        import android.content.Context;
-        import android.media.AudioManager;
-        import android.media.MediaPlayer;
-        import android.os.Bundle;
-        import android.view.LayoutInflater;
-        import android.view.View;
-        import android.view.ViewGroup;
-        import android.widget.AdapterView;
-        import android.widget.ListView;
-        import android.widget.TextView;
+import android.support.v4.app.Fragment;
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
 
-        import java.util.ArrayList;
+import java.util.ArrayList;
 
 /**
  * Created by Johnny on 12/8/2017.
